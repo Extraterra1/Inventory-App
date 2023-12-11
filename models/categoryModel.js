7bc0e2bbc4ffad66e6d458037741291287ba8025
Carlos Pires <extraterra1@gmail.com>
2023-12-11 12:34:32 +0000

    feat: category model

---
+++ models/categoryModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxLength: 20
+  },
+  desc: {
+    type: String,
+    required: true,
+    maxLength: 20
+  }
+});
+
+// Virtual for Category URL
+categorySchema.virtual('url').get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/category/${this._id}`;
+});
+
+module.exports = mongoose.model('Category', categorySchema);
