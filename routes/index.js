f7356e33e4e9685a046f9c89772856163d2cdb5f
Carlos Pires <extraterra1@gmail.com>
2023-12-12 09:45:11 +0000

    feat: fetch document count from db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../models/categoryModel');
+const Product = require('../models/productModel');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Welcome' });
+router.get('/', async function (req, res, next) {
+  const [categories, products] = await Promise.all([Category.find(), Product.find()]);
+  res.render('index', {
+    title: 'Welcome',
+    categories: categories.slice(0, 5),
+    products: products.slice(0, 5),
+    numCategories: categories.length,
+    numProducts: products.length
+  });
 });
 
 module.exports = router;
