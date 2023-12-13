6c0e3536a6997a152d7f06b814e605470527350b
Carlos Pires <extraterra1@gmail.com>
2023-12-13 13:07:25 +0000

    dev: category detail route

--- routes/categories.js
+++ routes/categories.js
@@ -2,17 +2,28 @@ const express = require('express');
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const Category = require('../models/categoryModel');
+const Product = require('../models/productModel');
 
 /* GET home page. */
 router.get(
   '/',
   asyncHandler(async (req, res, next) => {
     const categories = await Category.find({}).sort({ createdAt: 1 });
-    console.log(categories);
     res.render('categoriesOverview', {
       title: 'Categories',
       categories
     });
+  })
+);
+
+router.get(
+  '/:id',
+  asyncHandler(async (req, res, next) => {
+    const [category, products] = await Promise.all([
+      Category.findById(req.params.id).sort({ createdAt: 1 }),
+      Product.find({ category: req.params.id }).sort({ createdAt: 1 })
+    ]);
+    res.render('categoryDetail', { title: category.name, category, products });
   })
 );
 
