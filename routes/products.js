dd5f0a30f2ab613d2c4dc920a0bb4e79233a15d2
Carlos Pires <extraterra1@gmail.com>
2023-12-14 14:48:10 +0000

    feat: update product

--- routes/products.js
+++ routes/products.js
@@ -85,6 +85,52 @@ router.get(
   })
 );
 
+router.post('/:id/edit', [
+  body('name', 'Name must be between 3 and 20 chars long').trim().isLength({ min: 3, max: 20 }),
+  body('price')
+    .trim()
+    .isNumeric()
+    .withMessage('Price must be a number')
+    .isFloat({ min: 0.01 })
+    .withMessage('Price must be greater than 0')
+    .notEmpty()
+    .withMessage('Price cannot be empty'),
+  body('quantity')
+    .trim()
+    .isInt()
+    .withMessage('Quantity must be an integer')
+    .isInt({ min: 1 })
+    .withMessage('Quantity must be greater than 0')
+    .notEmpty()
+    .withMessage('Quantity cannot be empty'),
+  body('category', 'Invalid Category').trim().isMongoId(),
+  asyncHandler(async (req, res, next) => {
+    if (!ObjectId.isValid(req.params.id)) return next(new Error('Invalid ID'));
+    const errors = validationResult(req);
+    const newProduct = new Product({
+      name: req.body.name,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      category: req.body.category,
+      _id: req.params.id
+    });
+    const categories = await Category.find().sort({ name: 1 });
+    if (!errors.isEmpty()) {
+      const product = await Product.findById(req.params.id);
+      return res.render('productCreate', {
+        title: 'Edit ' + product.name,
+        product,
+        errors: errors.array(),
+        categories
+      });
+    }
+
+    await Product.findByIdAndUpdate(req.params.id, newProduct);
+
+    res.redirect(newProduct.url);
+  })
+]);
+
 router.post(
   '/:id/delete',
   asyncHandler(async (req, res, next) => {
